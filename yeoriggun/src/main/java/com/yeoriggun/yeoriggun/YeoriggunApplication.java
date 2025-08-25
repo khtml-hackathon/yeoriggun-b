3ebb86d1bc89f50428918e625c6591a8b3b2b5aa
RYU <rjw5256@gmail.com>
2025-08-25 18:55:48 +0900

    ✨ Feat: Add 프로젝트 생성
    프로젝트를 생성했습니다

---
+++ yeoriggun/src/main/java/com/yeoriggun/yeoriggun/YeoriggunApplication.java
@@ -0,0 +1,13 @@
+package com.yeoriggun.yeoriggun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeoriggunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeoriggunApplication.class, args);
+	}
+
+}
